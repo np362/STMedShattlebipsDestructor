ff0f2301a6d289343b74f1bddee9cb6562b938ce
np362 <np8643@mci4me.at>
2025-05-26 11:19:50 +0200

    Initialized main.c and main.h; added a .gitignore

---
+++ include/main.h
@@ -0,0 +1,2 @@
+#include <stm32f091xc.h>
+
---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <stm32f091xc.h>
+
+/*
+    @brief Declaration of all global variables
+*/
+
+/*
+    @brief main function
+*/
+int main(void)
+{
+    // main loop
+    for(;;){
+        // do something
+        asm("nop");
+    }
+    return 0;
+}
