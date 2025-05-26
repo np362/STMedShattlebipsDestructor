5e0e703de860e5a4908a1529b225d4458c5ca931
np362 <np8643@mci4me.at>
2025-05-26 11:35:49 +0200

    add: main(), int_shoot()/ only init, UART_INIT()

--- src/main.c
+++ src/main.c
@@ -1,11 +1,37 @@
 #include <stm32f091xc.h>
+#include <stdio.h>
 
-/*
+/**
     @brief Declaration of all global variables
 */
 
+/**
+    @brief Initialize UART
+*/
+void UART_INIT()
+{
+
+}
+
+
+/**
+   * @brief Algorithm for shooting 
+   * @return coords[array[10x10]], target coords x and y
+**/
+int int_shoot(int coords){
+    // coords is a array including the 10x10 field
+
+    // x,y coordinates of target
+    uint8_t x = 0;
+    uint8_t y = 0;
+
+    // return modified array with x and y of new target
+    return coords, x, y;
+}
+
 /*
     @brief main function
+    @return 0
 */
 int main(void)
 {
